c87336185cc5cc0de9a6a746f996fa6bd02a77c8
Miok Kim <kim00476@algonquinlive.com>
2023-09-27 23:56:26 -0400

    Finished Phone Version of Lab 3"

--- app/src/main/java/algonquin/cst2335/kim00476/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/kim00476/ui/MainActivity.java
@@ -6,6 +6,9 @@ import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -27,62 +30,82 @@ public class MainActivity extends AppCompatActivity {
 
     private MainViewModel model;
     private ActivityMainBinding variableBinding;
-
+    ImageView imgView;
+    Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        model = new ViewModelProvider(this).get(MainViewModel.class);
+        imgView = findViewById(R.id.flagView);
+        sw = findViewById(R.id.spin_switch);
 
-        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(variableBinding.getRoot());
+        sw.setOnCheckedChangeListener((btn, isChecked) -> {
+            if (isChecked) {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
 
-        TextView myText = variableBinding.textView;
-        Button btn = variableBinding.myButton;
-        EditText myEdit = variableBinding.myEditText;
-        CheckBox cb = variableBinding.myCheckBox;
-        Switch sw = variableBinding.mySwitch;
-        RadioButton rb = variableBinding.myRadioButton;
-        ImageView im = variableBinding.imageTest;
-        ImageButton ib = variableBinding.imageBtn;
-
-        btn.setOnClickListener(v -> {
-            String editString = myEdit.getText().toString();
-            model.editString.postValue(editString);
-        });
-
-        variableBinding.myCheckBox.setOnCheckedChangeListener( (btn1, isSelected) -> {
-            model.isSelected.postValue(isSelected);
-            Toast.makeText(getApplicationContext(), "The value is now: " + isSelected, Toast.LENGTH_SHORT).show();
-        } );
-
-        variableBinding.mySwitch.setOnCheckedChangeListener(  ( btn1, isSelected) -> {
-            model.isSelected.postValue(isSelected);
-            Toast.makeText(getApplicationContext(), "The value is now: " + isSelected, Toast.LENGTH_SHORT).show();
-        } );
-
-        variableBinding.myRadioButton.setOnCheckedChangeListener( ( btn1, isSeleted) -> {
-            model.isSelected.postValue(isSeleted);
-            Toast.makeText(this, "The value is now: " + isSeleted, Toast.LENGTH_SHORT).show();
-        } );
-
-        variableBinding.imageTest.setOnClickListener(clk -> {
-            Toast.makeText(this, "The width = " + variableBinding.imageTest.getWidth() +" "+ "and height = " +variableBinding.imageTest.getHeight(), Toast.LENGTH_SHORT).show();
-        });
-
-        variableBinding.imageBtn.setOnClickListener(clk -> {
-            Toast.makeText(this, "The width = " + variableBinding.imageBtn.getWidth() +" "+ "and height = " +variableBinding.imageBtn.getHeight(), Toast.LENGTH_SHORT).show();
-        });
-
-        model.isSelected.observe(this, selected -> {
-            variableBinding.myCheckBox.setChecked(selected);
-            variableBinding.myRadioButton.setChecked(selected);
-            variableBinding.mySwitch.setChecked(selected);
-        });
-
-        model.editString.observe(this, editString -> {
-            myText.setText("Your edit text has: " +  model.editString.getValue());
+                imgView.startAnimation(rotate);
+            } else {
+                imgView.clearAnimation();
+            }
         });
     }
 }
+//
+//        model = new ViewModelProvider(this).get(MainViewModel.class);
+//
+//        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(variableBinding.getRoot());
+//
+//        TextView myText = variableBinding.textView;
+//        Button btn = variableBinding.myButton;
+//        EditText myEdit = variableBinding.myEditText;
+//        CheckBox cb = variableBinding.myCheckBox;
+//        Switch sw = variableBinding.mySwitch;
+//        RadioButton rb = variableBinding.myRadioButton;
+//        ImageView im = variableBinding.imageTest;
+//        ImageButton ib = variableBinding.imageBtn;
+//
+//        btn.setOnClickListener(v -> {
+//            String editString = myEdit.getText().toString();
+//            model.editString.postValue(editString);
+//        });
+//
+//        variableBinding.myCheckBox.setOnCheckedChangeListener( (btn1, isSelected) -> {
+//            model.isSelected.postValue(isSelected);
+//            Toast.makeText(getApplicationContext(), "The value is now: " + isSelected, Toast.LENGTH_SHORT).show();
+//        } );
+//
+//        variableBinding.mySwitch.setOnCheckedChangeListener(  ( btn1, isSelected) -> {
+//            model.isSelected.postValue(isSelected);
+//            Toast.makeText(getApplicationContext(), "The value is now: " + isSelected, Toast.LENGTH_SHORT).show();
+//        } );
+//
+//        variableBinding.myRadioButton.setOnCheckedChangeListener( ( btn1, isSeleted) -> {
+//            model.isSelected.postValue(isSeleted);
+//            Toast.makeText(this, "The value is now: " + isSeleted, Toast.LENGTH_SHORT).show();
+//        } );
+//
+//        variableBinding.imageTest.setOnClickListener(clk -> {
+//            Toast.makeText(this, "The width = " + variableBinding.imageTest.getWidth() +" "+ "and height = " +variableBinding.imageTest.getHeight(), Toast.LENGTH_SHORT).show();
+//        });
+//
+//        variableBinding.imageBtn.setOnClickListener(clk -> {
+//            Toast.makeText(this, "The width = " + variableBinding.imageBtn.getWidth() +" "+ "and height = " +variableBinding.imageBtn.getHeight(), Toast.LENGTH_SHORT).show();
+//        });
+//
+//        model.isSelected.observe(this, selected -> {
+//            variableBinding.myCheckBox.setChecked(selected);
+//            variableBinding.myRadioButton.setChecked(selected);
+//            variableBinding.mySwitch.setChecked(selected);
+//        });
+//
+//        model.editString.observe(this, editString -> {
+//            myText.setText("Your edit text has: " +  model.editString.getValue());
+//        });
+//    }
+//}
